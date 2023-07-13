d91d6994108f54922c269f7b0268e590104f0a40
sh1fuo <139454715+sh1fuo@users.noreply.github.com>
2023-07-13 13:31:45 +0300

    Add files via upload

---
+++ personalrepository.rb
@@ -0,0 +1,13 @@
+﻿class Person
+  attr_accessor :name, :age, :gender
+
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+  def introduce
+    puts "Hi, my name is #{@name}. I am #{@age} years old and #{@gender}."
+  end
+end
